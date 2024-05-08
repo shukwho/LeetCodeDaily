cfbbf58eaf2598ca69d4c497f7f0053cd9d493af
shukwho <shsushmita95@gmail.com>
2024-05-08 11:32:42 +0530

    first commit

---
+++ src/main/java/com/who/shuk/Main.java
@@ -0,0 +1,7 @@
+package com.who.shuk;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/who/shuk/leetcodeDaily/RelativeRanks.java
@@ -0,0 +1,5 @@
+package com.who.shuk.leetcodeDaily;
+
+public class RelativeRanks {
+    //https://leetcode.com/problems/relative-ranks/description/
+}
